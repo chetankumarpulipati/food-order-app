e7cca4e9532ebad1b04617d64e3656ec8ca342ab
chetan kuamr <chetankumarpulipati4@gmail.com>
2024-05-25 08:19:34 +0530

    com

--- src/screens/HomeScreen/home.screen.tsx
+++ src/screens/HomeScreen/home.screen.tsx
@@ -11,33 +11,33 @@ import {
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
-  const imageUri = 'https://via.placeholder.com/150';
+  const myImage = require('C:\\Users\\cheta\\Desktop\\food-order-app\\src\\assets\\images\\pizza.jpg');
   const title = 'Title';
   const description = 'Description';
   const price = 10;
   
   return (
     <View style={styles.container}>
-        <View
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            borderBottomWidth: 1,
-            borderColor: '#ddd',
-            paddingVertical: 16,
-          }}>
+        <View style={styles.design}>
           <Image
-            source={{uri: imageUri}}
-            style={{width: 80, height: 80, borderRadius: 5, marginRight: 16}}
-          />
+            source={myImage}
+            style={{width: 80, height: 80, borderRadius: 5, marginRight: 16}} />
           <View style={{flex: 1}}>
             <Text style={{fontSize: 16, fontWeight: 'bold'}}>{title}</Text>
             <Text style={{fontSize: 14, color: '#999'}}>{description}</Text>
-            <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 5}}>
-              ${price}
-            </Text>
+            <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 5}}>${price}</Text>
           </View>
         </View>
+        <View style={styles.design}>
+        <Image
+            source={myImage}
+            style={{width: 80, height: 80, borderRadius: 5, marginRight: 16}} />
+        <View style={{flex: 1}}>
+          <Text style={{fontSize: 16, fontWeight: 'bold'}}>{title}</Text>
+          <Text style={{fontSize: 14, color: '#999'}}>{description}</Text>
+          <Text style={{fontSize: 16, fontWeight: 'bold', marginTop: 5}}>${price}</Text>
+        </View>
+      </View>
       </View>
   );
 };
@@ -47,17 +47,17 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  top: {
-    backgroundColor: '#c7eaf0',
-    width: '95%',
-    height: '6%',
-    margin: 10,
-    borderRadius: 10,
-    justifyContent: 'center',
-    paddingLeft: 10,
+    padding: 10,
+    margin: 3,
   },
   map: {
     flex: 1,
+  },
+  design: {
+    display: 'flex',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
   },
 });
